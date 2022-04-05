0c38a3a2d8734e448700841fca5d28655b65f0f2
Sal Rahman <salehen.rahman@gmail.com>
2022-04-05 10:13:20 -0700

    Some refactoring

--- sphere.v
+++ sphere.v
@@ -13,14 +13,14 @@ fn (s Sphere) hit(r Ray, t_min f32, t_max f32, mut rec HitRecord) bool {
 	c := oc.dot(oc) - s.radius * s.radius
 	discriminant := b*b - a*c
 	if discriminant > 0 {
-		mut temp := -b - math.sqrt(b*b-a*c)/a
+		mut temp := -b - math.sqrt((b*b) - (a*c))/a
 		if temp < t_max && temp > t_min {
 			rec.t = f32(temp)
 			rec.p = r.point_at_parameter(rec.t)
 			rec.normal = rec.p.sub(s.center).scalar_div(s.radius)
 			return true
 		}
-		temp = (-b + math.sqrt(b*b-a*c))/a
+		temp = (-b + math.sqrt((b*b) - (a*c)))/a
 		if temp < t_max && temp > t_min {
 			rec.t = f32(temp)
 			rec.p = r.point_at_parameter(rec.t)
