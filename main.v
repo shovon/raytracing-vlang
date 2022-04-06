3072e92a966c008f28daf66dca3c9507d68e3c8a
Sal Rahman <salehen.rahman@gmail.com>
2022-04-05 19:34:32 -0700

    Done. Finito

--- main.v
+++ main.v
@@ -19,31 +19,80 @@ fn color(r Ray, world Hitable, depth int) Vec3 {
 		.add(Vec3{0.5, 0.7, 1.0}.scalar_mul(t))
 }
 
+fn random_scene() Hitable {
+	// n := 500
+	mut list := []Hitable{}
+	
+	mat1 := &Lambertian{Vec3{0.5, 0.5, 0.5}}
+	list << &Sphere{Vec3{0, -1000, 0}, 1000, mat1}
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			choose_mat := rand.f32()
+			center := Vec3{a+0.9*rand.f32(),0.2,b+0.9*rand.f32()}
+			if center.sub(Vec3{4,0.2,0}).length() > 0.9 {
+				if choose_mat < 0.8 {
+					mat := &Lambertian{Vec3{
+						rand.f32()*rand.f32(),
+						rand.f32()*rand.f32(),
+						rand.f32()*rand.f32()
+					}}
+					list << &Sphere{center, 0.2, mat}
+				} else if choose_mat < 0.95 {
+					mat := &Metal{
+						Vec3{
+							0.5*(1+rand.f32()),
+							0.5*(1+rand.f32()),
+							0.5*(1+rand.f32())
+						},
+						0.5*rand.f32()
+					}
+					list << &Sphere{center, 0.2, mat}
+				} else {
+					mat := &Dialectric{1.5}
+					list << &Sphere{center, 0.2, mat}
+				}
+			}
+		}
+	}
+
+	mat2 := &Dialectric{1.5}
+	list << &Sphere{Vec3{0, 1, 0}, 1, mat2}
+	
+	mat3 := &Lambertian{Vec3{0.4,0.2,0.1}}
+	list << &Sphere{Vec3{-4, 1, 0}, 1, mat3}
+
+	mat4 := &Metal{Vec3{0.7,0.6,0.5},0.0}
+	list << &Sphere{Vec3{5, 1, 0}, 1, mat4}
+
+	return &HitableList{list}
+}
+
 fn main() {
-	nx := 200
-	ny := 100
+	nx := 1024
+	ny := 576
 	ns := 100
 
 	print('P3\n$nx $ny\n255\n')
 
-	mut list := []Hitable{}
-	vec1 := Vec3{0.1, 0.2, 0.5}
-	mat1 := &Lambertian{vec1}
-	list << &Sphere{Vec3{0,0,-1}, 0.5, mat1}
+	// mut list := []Hitable{}
+	// vec1 := Vec3{0.1, 0.2, 0.5}
+	// mat1 := &Lambertian{vec1}
+	// list << &Sphere{Vec3{0,0,-1}, 0.5, mat1}
 
-	vec2 := Vec3{0.8, 0.8, 0.0}
-	mat2 := &Lambertian{vec2}
-	list << &Sphere{Vec3{0,-100.5,-1}, 100, mat2}
+	// vec2 := Vec3{0.8, 0.8, 0.0}
+	// mat2 := &Lambertian{vec2}
+	// list << &Sphere{Vec3{0,-100.5,-1}, 100, mat2}
 
-	vec3 := Vec3{0.8, 0.6, 0.2}
-	mat3 := &Metal{vec3, 0.3}
-	list << &Sphere{Vec3{1,0,-1}, 0.5, mat3}
+	// vec3 := Vec3{0.8, 0.6, 0.2}
+	// mat3 := &Metal{vec3, 0.3}
+	// list << &Sphere{Vec3{1,0,-1}, 0.5, mat3}
 
-	mat4 := &Dialectric{1.5}
-	list << &Sphere{Vec3{-1,0,-1}, 0.5, mat4}
+	// mat4 := &Dialectric{1.5}
+	// list << &Sphere{Vec3{-1,0,-1}, 0.5, mat4}
 
-	mat5 := &Dialectric{1.5}
-	list << &Sphere{Vec3{-1,0,-1}, -0.45, mat5}
+	// mat5 := &Dialectric{1.5}
+	// list << &Sphere{Vec3{-1,0,-1}, -0.45, mat5}
 
 	// radius := f32(math.cos(math.pi / 4))
 	// vec1 := Vec3{0, 0, 1}
@@ -54,13 +103,14 @@ fn main() {
 	// mat2 := &Lambertian{vec2}
 	// list << &Sphere{Vec3{radius,0,-1}, radius, mat2}
 
-	world := HitableList{list}
+	// world := HitableList{list}
+	world := random_scene()
 
-	lookfrom := Vec3{3, 3, 2}
-	lookat := Vec3{0, 0, -1}
+	lookfrom := Vec3{12, 2, 4}
+	lookat := Vec3{0, 1, 0}
 	dist_to_focus := lookfrom.sub(lookat).length()
-	aperture := f32(2.0)
-	cam := new_camera(lookfrom, lookat, Vec3{0, 1, 0}, 29, f32(nx)/f32(ny), aperture, dist_to_focus)
+	aperture := f32(0.2)
+	cam := new_camera(lookfrom, lookat, Vec3{0, 1, 0}, 20, f32(nx)/f32(ny), aperture, dist_to_focus)
 
 	for j := ny-1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
